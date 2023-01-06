9736ee9d12044e80983ee983d759805cfac84f12
bmitc <65685447+bmitc@users.noreply.github.com>
2023-01-06 02:28:15 -0500

    Add `is_mapset` guard tests

--- test/utilities_test.exs
+++ test/utilities_test.exs
@@ -1,4 +1,23 @@
 defmodule UtilitiesTest do
   use ExUnit.Case
   doctest Utilities, import: true
+
+  import Utilities, only: [is_mapset: 1]
+
+  describe "MapSet guard" do
+    test "empty MapSet" do
+      assert is_mapset(%MapSet{})
+      assert is_mapset(MapSet.new())
+    end
+
+    test "non-empty MapSet" do
+      assert is_mapset(MapSet.new([1, 2, 3]))
+      assert is_mapset(MapSet.new([:a, :b, :c], &Atom.to_string/1))
+    end
+
+    test "on regular map" do
+      refute is_mapset(%{})
+      refute is_mapset(%{a: 1, b: 3})
+    end
+  end
 end
